1ed7f0869c936e98a36f06658088e82773ecf6fd
TheFlatTenorSax <98133969+TheFlatTenorSax@users.noreply.github.com>
2022-01-25 18:30:42 -0800

    Eightball Code

--- Eightball.java
+++ Eightball.java
@@ -16,7 +16,7 @@ public class EightBall {
         //Boolean variable to handle the loop
         boolean loopCheck = true;
 
-        //Array which includes all the possible 8Ball outputs
+        //Includes all the possible 8Ball outputs
         String [] arr = {"It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."};
         
         //Selects a random output
